bf3d45745782b304593d09b5e2d7a105fe85d4f1
iSquirrel <344900432@qq.com>
2018-05-01 21:36:30 +0800

    update fabfile.py-add version tag contron

--- growth_studio/fabfile.py
+++ growth_studio/fabfile.py
@@ -20,3 +20,14 @@ def hello():
 def pep8():
     """CHECK THE CODE STYLE FOR PEP8"""
     local('pep8 .')
+
+@task
+def tag_version(version):
+    """Tag NEW VERSION"""
+    local("git tag %s" % version)
+    local("git push origin %s" % version)
+
+@task
+def fetch_version(version):
+    "Fetch Git Version"
+    local('wget https://github.com/KFGameHacker/pythonPlayground/archive/%s.zip'%version)
